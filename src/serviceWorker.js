8661f9a3e987a480f40b3664eee9f3fc707a0080
Denner Rondinely <denner.rondinely@gmail.com>
2020-07-23 08:52:27 -0300

    Configurar PWA

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,8 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-console */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -139,3 +144,47 @@ export function unregister() {
       });
   }
 }
+
+const CACHE_NAME = 'pwa-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/js/main.chunk.js',
+  '/static/js/1.chunk.js',
+  '/static/js/0.chunk.js',
+  '/favicon.ico',
+  '/css?family=Open+Sans',
+  '/icon?family=Material+Icons',
+];
+
+window.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+window.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+window.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
